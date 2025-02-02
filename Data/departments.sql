2220c417d438527a5685674673a0b88e24322038
vishal signh <vishalsinghomr@gmail.com>
2025-02-02 21:31:28 +0530

    departments

---
+++ Data/departments.sql
@@ -0,0 +1,12 @@
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100)
+);
+
+INSERT INTO departments (department_id, department_name) VALUES
+(1, 'Parks and Recreation'),
+(2, 'Animal Control'),
+(3, 'Public Works'),
+(4, 'Healthcare'),
+(5, 'Library'),
+(6, 'Finance');
